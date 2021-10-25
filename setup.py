6c2a8f6933fe12c8b5be7156961437889250333e
Mihail-Cosmin Munteanu <munteanumihailcosmin@gmail.com>
2021-10-25 14:26:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
+requirements = ['pyautogui',
+                'pillow'
 ]
 
 test_requirements = ['pytest>=3', ]
